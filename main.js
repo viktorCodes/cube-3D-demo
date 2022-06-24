e5c5aaa91a25b35701a3217f5054b97339ec2d21
calimendoza <mbaivictor1@gmail.com>
2022-06-24 06:02:45 -0700

    added to main.js

--- main.js
+++ main.js
@@ -35,4 +35,80 @@ function main() {
  }
 `;
   
+const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+    },
+  };
+
+  function initBuffers(gl) {
+
+    // Create a buffer for the square's positions.
+  
+    const positionBuffer = gl.createBuffer();
+  
+    // Select the positionBuffer as the one to apply buffer
+    // operations to from here out.
+  
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  
+    // Now create an array of positions for the square.
+  
+    const positions = [
+       1.0,  1.0,
+      -1.0,  1.0,
+       1.0, -1.0,
+      -1.0, -1.0,
+    ];
+
+    // Now pass the list of positions into WebGL to build the
+  // shape. We do this by creating a Float32Array from the
+  // JavaScript array, then use it to fill the current buffer.
+
+  gl.bufferData(gl.ARRAY_BUFFER,
+    new Float32Array(positions),
+    gl.STATIC_DRAW);
+
+return {
+position: positionBuffer,
+};
+  
+
+}  
+
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+
+
+
+}
+
+
   window.onload = main;
