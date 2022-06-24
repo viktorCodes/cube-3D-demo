9a660a8358b367c7f4baa6fdf2633ed3406a9f33
calimendoza <mbaivictor1@gmail.com>
2022-06-24 00:57:21 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+function main() {
+    const canvas = document.querySelector("#glCanvas");
+    // Initialize the GL context
+    const gl = canvas.getContext("webgl");
+  
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+    }
+  
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  }
+
+   // Vertex shader program
+
+   const vsSource = `
+   attribute vec4 aVertexPosition;
+
+   uniform mat4 uModelViewMatrix;
+   uniform mat4 uProjectionMatrix;
+
+   void main() {
+     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+   }
+ `;
+
+ const fsSource = `
+ void main() {
+   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+ }
+`;
+  
+  window.onload = main;
